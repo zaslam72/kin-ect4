0b2b209e6056c190393ec171773b143bb42f8916
Your Name <kelvin.lightner@gmail.com>
2016-11-17 11:12:23 -0800

    Sessions / Users new routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,9 @@
 class SessionsController < ApplicationController
-  def index
-  end
 
   def new
   end
 
   def create
-  end
-
-  def show
-  end
-
-  def edit
-  end
-
-  def update
   end
 
   def destroy
