f9a3d6367b2dcfbaa02745155093315a8172934d
Alexis Carroll <lex.ac1@gmail.com>
2016-11-17 10:27:09 -0800

    Add all routes needed

--- config/routes.rb
+++ config/routes.rb
@@ -1,73 +1,28 @@
 Rails.application.routes.draw do
-  get 'sessions/index'
 
-  get 'sessions/new'
 
-  get 'sessions/create'
+  resources :sessions, only: [:new, :create, :destroy]
 
-  get 'sessions/show'
+  resources :categories, only: [:index, :show] do
+    resources :games, only: [:index, :show]
+  end
 
-  get 'sessions/edit'
+  # games/results
+  resources :games, only: [:index, :show] do
+    resources :comments
+    resources :votes, except: [:index, :destroy]
+    # need to update votes
+    resources :users
+  end
 
-  get 'sessions/update'
+  resources :users, except: [:index] do
+    resources :comments
+    resources :votes, except: [:index, :destroy]
+    resources :games, except: [:edit, :update]
+    resources :friends, except: [:edit, :update]
+  end
 
-  get 'sessions/destroy'
-
-  get 'users/index'
-
-  get 'users/new'
-
-  get 'users/create'
-
-  get 'users/show'
-
-  get 'users/edit'
-
-  get 'users/update'
-
-  get 'users/destroy'
-
-  get 'games/index'
-
-  get 'games/new'
-
-  get 'games/create'
-
-  get 'games/show'
-
-  get 'games/edit'
-
-  get 'games/update'
-
-  get 'games/destroy'
-
-  get 'votes/index'
-
-  get 'votes/new'
-
-  get 'votes/create'
-
-  get 'votes/show'
-
-  get 'votes/edit'
-
-  get 'votes/update'
-
-  get 'votes/destroy'
-
-  get 'categories/index'
-
-  get 'categories/new'
-
-  get 'categories/create'
-
-  get 'categories/show'
-
-  get 'categories/edit'
-
-  get 'categories/update'
-
-  get 'categories/destroy'
+  root 'games#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
