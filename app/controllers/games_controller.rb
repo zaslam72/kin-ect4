8a2e1d34805eb36aec727021da6ee85134238ed2
Long Tran <artsandcrass@gmail.com>
2016-11-17 16:15:59 -0800

    Add subscription button

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -14,4 +14,13 @@ class GamesController < ApplicationController
     @games = Game.where(category_id: params[:category_id])
     render :index
   end
+
+  def new
+    @game = Game.new
+  end
+
+  def create
+    @subscription = Subscription.create(user_id: current_user.id, game_id: params[:id])
+    redirect_to game_path
+  end
 end
